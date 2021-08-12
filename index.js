1ccf19c1e9fb500ff2de526d2801f7f29080b196
Courtney Cash <cashcourtney97@gmail.com>
2021-08-12 11:50:42 -0400

    commit tree 906e61f513fed43e7bae611b9b9cfc4204d1c33f

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+// // const titleCase = (str) => {
+// //  return str.toLowerCase().split(' ').map(function(word) {
+
+// })
+// }
+
 const titleCased = () => {
-  return tutorials
+  const title = tutorials.map(function (name){
+    const words = name.split (' ');
+    const combine = words.map(word => word[0].toUpperCase() + word.slice(1));
+    const cased = combine.join (' ');
+    return cased;
+  });
+    return title;
 }
+
+
+console.log(title)
+
+// const titleCased = (input) => {
+//   return tutorials.map( line => {
+//     const tokens = line.split(' ');
+//     const capitalizedTokens =
+//       tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) );
+//     const response = capitalizedTokens.join(' ');
+//     return response;
+//   });
+// }
+
+
+// to denote the first letter of each first word in each title you can use the index. for all other words you can use needs to be a space before 
